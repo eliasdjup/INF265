c9ce22ef9fade4ab8c5403e944424c49036408aa
Elias Djupesland <elias.djupesland@autostoresystem.com>
2022-02-16 18:33:52 +0100

    Task 1

--- Project1/tests_backpropagation.py
+++ Project1/tests_backpropagation.py
@@ -2,50 +2,49 @@ import torch
 from torch import nn, optim
 from torchvision import datasets, transforms
 
+
 class MyNet(nn.Module):
-    def __init__(self, n_l = [2, 3, 2]):
+    def __init__(self, n_l=[2, 3, 2]):
         super().__init__()
 
-
         # number of layers in our network (following Andrew's notations)
-        self.L = len(n_l)-1
+        self.L = len(n_l) - 1
         self.n_l = n_l
 
         # Where we will store our neuron values
         # - z: before activation function
         # - a: after activation function (a=f(z))
-        self.z = {i : None for i in range(1, self.L+1)}
-        self.a = {i : None for i in range(self.L+1)}
+        self.z = {i: None for i in range(1, self.L + 1)}
+        self.a = {i: None for i in range(self.L + 1)}
 
         # Where we will store the gradients for our custom backpropagation algo
-        self.dL_dw = {i : None for i in range(1, self.L+1)}
-        self.dL_db = {i : None for i in range(1, self.L+1)}
+        self.dL_dw = {i: None for i in range(1, self.L + 1)}
+        self.dL_db = {i: None for i in range(1, self.L + 1)}
 
         # Our activation functions
-        self.f = {i : lambda x : torch.tanh(x) for i in range(1, self.L+1)}
+        self.f = {i: lambda x: torch.tanh(x) for i in range(1, self.L + 1)}
 
         # Derivatives of our activation functions
         self.df = {
-            i : lambda x : (1 / (torch.cosh(x)**2))
-            for i in range(1, self.L+1)
+            i: lambda x: (1 / (torch.cosh(x) ** 2)) for i in range(1, self.L + 1)
         }
 
         # fully connected layers
         # We have to use nn.ModuleDict and to use strings as keys here to
         # respect pytorch requirements (otherwise, the model does not learn)
-        self.fc = nn.ModuleDict({str(i): None for i in range(1, self.L+1)})
-        for i in range(1, self.L+1):
-            self.fc[str(i)] = nn.Linear(in_features=n_l[i-1], out_features=n_l[i])
+        self.fc = nn.ModuleDict({str(i): None for i in range(1, self.L + 1)})
+        for i in range(1, self.L + 1):
+            self.fc[str(i)] = nn.Linear(in_features=n_l[i - 1], out_features=n_l[i])
 
     def forward(self, x):
         # Input layer
         self.a[0] = torch.flatten(x, 1)
 
         # Hidden layers until output layer
-        for i in range(1, self.L+1):
+        for i in range(1, self.L + 1):
 
             # fully connected layer
-            self.z[i] = self.fc[str(i)](self.a[i-1])
+            self.z[i] = self.fc[str(i)](self.a[i - 1])
             # activation
             self.a[i] = self.f[i](self.z[i])
 
@@ -64,11 +63,11 @@ def grad_check(model, x, y, loss_fn, eps=10e-5):
     with torch.no_grad():
 
         # Go through all layers
-        for i_layer in range(1, model.L+1):
+        for i_layer in range(1, model.L + 1):
             # Each layer has a weight and bias parameter
-            for param_name in ['weight', 'bias']:
+            for param_name in ["weight", "bias"]:
 
-                param =  getattr(model.fc[str(i_layer)], param_name)
+                param = getattr(model.fc[str(i_layer)], param_name)
                 if len(param.data.shape) == 1:
                     j_range = 1
                 else:
@@ -78,9 +77,9 @@ def grad_check(model, x, y, loss_fn, eps=10e-5):
                     for j in range(j_range):
 
                         if len(param.data.shape) == 1:
-                            idx = (i)
+                            idx = i
                         else:
-                            idx = (i,j)
+                            idx = (i, j)
 
                         # Compute loss with param += eps
                         model1 = MyNet(model.n_l)
@@ -97,8 +96,8 @@ def grad_check(model, x, y, loss_fn, eps=10e-5):
                         loss2 = loss_fn(model2(x), y).sum().item()
 
                         # Gradients computed with backprop and autograd
-                        grad_approx.append( (loss1 - loss2)/(2*eps) )
-                        if param_name == 'bias':
+                        grad_approx.append((loss1 - loss2) / (2 * eps))
+                        if param_name == "bias":
                             grad_backprop.append(model.dL_db[i_layer][idx])
                         else:
                             grad_backprop.append(model.dL_dw[i_layer][idx])
@@ -106,33 +105,33 @@ def grad_check(model, x, y, loss_fn, eps=10e-5):
         g_approx = torch.tensor(grad_approx)
         g_backprop = torch.tensor(grad_backprop)
 
-        res_backprop = (
-            torch.norm(g_approx - g_backprop)
-            / (torch.norm(g_approx) + torch.norm(g_backprop))
+        res_backprop = torch.norm(g_approx - g_backprop) / (
+            torch.norm(g_approx) + torch.norm(g_backprop)
         )
         if res_backprop > eps:
             print("RES BACKPROP", res_backprop)
-            flag=False
+            flag = False
     return flag
 
 
-def load_MNIST(data_path='../data/', preprocessor=None):
+def load_MNIST(data_path="../data/", preprocessor=None):
 
     if preprocessor is None:
-        preprocessor = transforms.Compose([
-            transforms.CenterCrop(24),
-            transforms.ToTensor(),
-            transforms.Normalize(0.1306, 0.3080),
-        ])
+        preprocessor = transforms.Compose(
+            [
+                transforms.CenterCrop(24),
+                transforms.ToTensor(),
+                transforms.Normalize(0.1306, 0.3080),
+            ]
+        )
 
     # load just test dataset because it's smaller
     data_test = datasets.MNIST(
-        data_path,
-        train=False,
-        download=True,
-        transform=preprocessor)
+        data_path, train=False, download=True, transform=preprocessor
+    )
 
     return data_test
+
 
 def check_computational_graph(model):
     """
@@ -140,12 +139,12 @@ def check_computational_graph(model):
     """
     res = True
     # Go through all layers
-    for i_layer in range(1, model.L+1):
+    for i_layer in range(1, model.L + 1):
         # Each layer has a weight and bias parameter
-        for param_name in ['weight', 'bias']:
+        for param_name in ["weight", "bias"]:
 
             # 'getattr(object, string variable)' is like `object.myattribute` when variable = "myattribute"
-            param =  getattr(model.fc[str(i_layer)], param_name)
+            param = getattr(model.fc[str(i_layer)], param_name)
             msg = " !!!! WARNING !!!!\nmodel.fc[" + str(i_layer) + "]." + param_name
             if not param.requires_grad:
                 print(msg + " does not require grad!")
@@ -158,42 +157,69 @@ def check_computational_graph(model):
     if res:
         print("All parameters seem correctly attached to the computational graph! :) ")
 
+
 def relative_error(a, b):
-    return (torch.norm(a - b) / torch.norm(a))
+    return torch.norm(a - b) / torch.norm(a)
+
 
 def compare_with_autograd(model, verbose=False):
     """
     Compare our gradients with autograd's computations
     """
-    flag=True
+    flag = True
     if verbose:
         print("\n ------------ fc['1'].weight.grad ------------ ")
         print("  Our computation:\n", model.dL_dw[1])
-        print("  Autograd's computation:\n", model.fc['1'].weight.grad)
+        print("  Autograd's computation:\n", model.fc["1"].weight.grad)
         print("\n ------------- fc['1'].bias.grad ------------- ")
         print("  Our computation:\n", model.dL_db[1])
-        print("  Autograd's computation:\n", model.fc['1'].bias.grad)
+        print("  Autograd's computation:\n", model.fc["1"].bias.grad)
     msg = "\n ------------- relative error ------------ \n"
     for i in range(1, model.L + 1):
         err = relative_error(model.fc[str(i)].weight.grad, model.dL_dw[i])
         if err > 0.001 or verbose:
-            print(msg + "(fc[" + str(i) + "].weight.grad, model.dL_dw[" + str(i) + "]):   %.4f" %err)
+            print(
+                msg
+                + "(fc["
+                + str(i)
+                + "].weight.grad, model.dL_dw["
+                + str(i)
+                + "]):   %.4f" % err
+            )
             flag = False
             msg = ""
         err = relative_error(model.fc[str(i)].bias.grad, model.dL_db[i])
         if err > 0.001 or verbose:
-            print(msg + "(fc[" + str(i) + "].bias.grad, model.dL_db[" + str(i) + "]):   %.4f" %err)
+            print(
+                msg
+                + "(fc["
+                + str(i)
+                + "].bias.grad, model.dL_db["
+                + str(i)
+                + "]):   %.4f" % err
+            )
             flag = False
             msg = ""
     return flag
 
-def check_gradients(model, backprop_fn, optimizer, loss_fn, x, y_true, n_epochs=5, eps=10e-5, verbose=False):
+
+def check_gradients(
+    model,
+    backprop_fn,
+    optimizer,
+    loss_fn,
+    x,
+    y_true,
+    n_epochs=5,
+    eps=10e-5,
+    verbose=False,
+):
     """
     Training loop that compares our gradients with autograd's computations
     """
     model.train()
 
-    for epoch in range(1, n_epochs+1):
+    for epoch in range(1, n_epochs + 1):
 
         grad_check_ok = True
         compare_autograd_ok = True
@@ -206,22 +232,25 @@ def check_gradients(model, backprop_fn, optimizer, loss_fn, x, y_true, n_epochs=
             loss.sum().backward()
 
             backprop_fn(model, out_expected, out)
-            grad_check_ok &= grad_check(model, input, out_expected, loss_fn,eps=eps)
+            grad_check_ok &= grad_check(model, input, out_expected, loss_fn, eps=eps)
 
             optimizer.step()
 
-            if i == (len(x)-1) and (epoch % 1 == 0):
-                print("\n ================= Epoch %d ================= "%epoch)
+            if i == (len(x) - 1) and (epoch % 1 == 0):
+                print("\n ================= Epoch %d ================= " % epoch)
 
                 compare_autograd_ok &= compare_with_autograd(model, verbose=verbose)
                 if compare_autograd_ok:
                     print("Gradients consistent with autograd's computations. :) ")
                 if grad_check_ok:
-                    print("Gradients consistent with finite differences computations. :) ")
+                    print(
+                        "Gradients consistent with finite differences computations. :) "
+                    )
 
             optimizer.zero_grad()
 
-def main_test(backprop_fn, model, eps=10e-5, verbose=False, data='toy'):
+
+def main_test(backprop_fn, model, eps=10e-5, verbose=False, data="toy"):
     """
     Main test function
 
@@ -232,44 +261,58 @@ def main_test(backprop_fn, model, eps=10e-5, verbose=False, data='toy'):
     - checking that all trainable parameters are still attached to
     the computational graph.
     """
-    fc1_w_init = model.fc['1'].weight.data
-    fc1_b_init = model.fc['1'].bias.data
+    fc1_w_init = model.fc["1"].weight.data
+    fc1_b_init = model.fc["1"].bias.data
 
     # Using a few images of the MNIST dataset
-    if data=='mnist':
-        if (model.n_l[0] != 24*24):
-            print('Please choose 576 (=24x24) as input dimension if using MNIST dataset')
+    if data == "mnist":
+        if model.n_l[0] != 24 * 24:
+            print(
+                "Please choose 576 (=24x24) as input dimension if using MNIST dataset"
+            )
         data = load_MNIST()
-        N=5
+        N = 5
         inputs = [torch.unsqueeze(img, 0).to(dtype=torch.double) for img, _ in data][:N]
         outputs = [torch.Tensor([[label]]) for _, label in data][:N]
         n_epochs = 1
     # Create toy dataset
     else:
         N = 10
-        inputs = torch.stack([i+torch.randn((1, model.n_l[0])) for i in range(N)])
-        outputs = torch.stack([torch.Tensor([[(-1)**j*i for j in range(model.n_l[-1])]]) for i in range(N)])
+        inputs = torch.stack([i + torch.randn((1, model.n_l[0])) for i in range(N)])
+        outputs = torch.stack(
+            [
+                torch.Tensor([[(-1) ** j * i for j in range(model.n_l[-1])]])
+                for i in range(N)
+            ]
+        )
         n_epochs = 5
-
 
     print("\n ========================  Check gradients ======================== ")
     loss_fn = nn.MSELoss(reduction="none")
     optimizer = optim.SGD(model.parameters(), lr=0.1)
     # Training loop that compares our gradients with autograd's computations
-    check_gradients(model, backprop_fn, optimizer, loss_fn, inputs, outputs, eps=eps, n_epochs=n_epochs, verbose=verbose)
-
+    check_gradients(
+        model,
+        backprop_fn,
+        optimizer,
+        loss_fn,
+        inputs,
+        outputs,
+        eps=eps,
+        n_epochs=n_epochs,
+        verbose=verbose,
+    )
 
     print("\n ==============  Check that weights have been updated ============= ")
     update_weight_ok = True
-    update_weight_ok &= not torch.all((fc1_w_init-model.fc['1'].weight.data).bool())
-    update_weight_ok &= not torch.all((fc1_b_init-model.fc['1'].bias.data).bool())
+    update_weight_ok &= not torch.all((fc1_w_init - model.fc["1"].weight.data).bool())
+    update_weight_ok &= not torch.all((fc1_b_init - model.fc["1"].bias.data).bool())
 
     if verbose:
-        print(model.fc['1'].weight.data)
-        print(model.fc['1'].bias.data)
+        print(model.fc["1"].weight.data)
+        print(model.fc["1"].bias.data)
     if update_weight_ok:
         print("Weights have been updated. :)")
 
     print("\n ===================  Check computational graph =================== ")
     check_computational_graph(model)
-
